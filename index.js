e5be8282abcdb5f05d8596c2fc833f14959e30ad
jiangtao <13832759+hustjiangtao@users.noreply.github.com>
2019-01-28 18:04:08 +0800

    Update: serve mode && switch logic adjustment
    Logic adjustment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const serve_mode = ['s', 'serve', 'server'].indexOf(hexo.env.args._[0]) != -1;
 const enable_baidu_push = hexo.config.baidu_url_submit.enable;
-if (!serve_mode || enable_baidu_push) {
+if (!serve_mode && enable_baidu_push) {
   hexo.extend.generator.register('baidu_url_generator', require('./lib/generator'));
   hexo.extend.deployer.register('baidu_url_submitter', require('./lib/submitter'));
   hexo.extend.deployer.register('baidu_xz_url_submitter', require('./lib/xz_submitter'));
